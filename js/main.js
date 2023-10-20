d2ce99a5feb0186a82ea0013cf88d999ab3d4f51
Aidan <38162@ma-web.nl>
2023-10-20 12:20:47 +0200

    haiku gemaakt

--- js/main.js
+++ js/main.js
@@ -9,9 +9,14 @@ function basicHaiku()
     return ["running to the shade","We saw a batman descending","In a basement with big D randy."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
+function student1Haiku(){
+    return["Auspicious autumn", "A single, solid cow hops", "above the chicken"]
+}
 
 haikus = [
-    basicHaiku()
+    basicHaiku(),
+    student1Haiku()
+    
 ]
 
 function randomHaiku()
